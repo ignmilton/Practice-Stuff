079651c3a6b83ee3b34e5b4e6486856a54c04552
ignmilton <ignmilton@gmail.com>
2022-01-06 21:52:55 +0530

    Removed Unnecessary code from Problem 2

--- EulerArchiveProblems/EulerArchiveProblem2.py
+++ EulerArchiveProblems/EulerArchiveProblem2.py
@@ -1,34 +1,3 @@
-"""
-import time
-start_time = time.time()
-
-fibonacciList = []
-fibonacciList.append(0)
-fibonacciList.append(1)
-
-k = 0
-n = 2
-
-while k <= 4_000_000:
-    k = fibonacciList[n-1] + fibonacciList[n-2]
-    fibonacciList.append(k)
-    n = n + 1
-   # print(k)
-fibonacciList.pop()
-
-sum = 0
-
-for q in range(0,len(fibonacciList)):
-    if fibonacciList[q]%2 == 0:
-        sum = sum + fibonacciList[q]
-    else:
-        sum = sum + 0
-
-print("The sum is: " + str(sum)) 
-
-print("Process finished --- %s seconds ---" % (time.time() - start_time))
-"""
-
 import time
 start_time = time.time()
 
